cb3813d774530fcb17f1a9d4abe3bd3bc340d6ee
swatijadhav <swati.jadhav@kiprosh.com>
2014-06-20 20:03:07 +0530

    blogger app

---
+++ js/helpers.js
@@ -0,0 +1,9 @@
+var showdown = new Showdown.converter();
+
+Ember.Handlebars.helper('format-markdown', function(input) {
+  return new Handlebars.SafeString(showdown.makeHtml(input));
+});
+
+Ember.Handlebars.helper('format-date', function(date) {
+  return moment(date).fromNow();
+});
